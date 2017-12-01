7e7d3241856ec5d06b571c6deedcaa34cb911993
Catherin Ortiz <catherinortiz@gmail.com>
2017-11-30 22:27:07 -0300

    Codigo del Arduino

---
+++ dhtjava/dhtjava.ino
@@ -0,0 +1,29 @@
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+    return;
+  }
+   
+  String datos = "dht;" + (String) temperature + ";" + (String) humidity + ";#";
+
+  Serial.println(datos);
+
+  //delay(1500);
+}
